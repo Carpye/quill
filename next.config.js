9c3a4d22fa71b4764ae452aef76c7db0cfef46b9
Kacper Kozłowski <kati8ko@gmail.com>
2023-11-12 02:11:06 +0100

    navbar account managing

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
     config.resolve.alias.encoding = false
     return config
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
